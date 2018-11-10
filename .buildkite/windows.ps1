4269f3388fb25d8cac639788057be0941c3ec491
Moritz Angermann <moritz.angermann@gmail.com>
2018-11-10 23:49:49 +0800

    :(

--- .buildkite/windows.ps1
+++ .buildkite/windows.ps1
@@ -2,4 +2,4 @@ Set-PSDebug -Trace 1
 
 $env:PATH="$env:PATH;D:\ghc\ghc-8.2.2\bin;D:\stack"
 
-C:\msys64\usr\bin\mintty.exe -w hide /bin/env MSYSTEM=MINGW64 /bin/bash -l build.sh
+stack.exe exec --no-ghc-package-path mintty -- -w hide /bin/env MSYSTEM=MINGW64 /bin/bash -l build.sh
