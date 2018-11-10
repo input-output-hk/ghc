7b7a52b5b94614cfd4254e12673d1fbe8e05487f
Moritz Angermann <moritz.angermann@gmail.com>
2018-11-11 00:03:35 +0800

    Really?

--- .buildkite/windows.ps1
+++ .buildkite/windows.ps1
@@ -2,7 +2,7 @@ Set-PSDebug -Trace 1
 
 $env:PATH="$env:PATH;D:\ghc\ghc-8.2.2\bin;D:\stack"
 
-choco install msys2
+choco install msys2 -y
 # choco install msys2 --params="'/InstallDir:C:\msys64'"
 
 mintty.exe /bin/env MSYSTEM=MINGW64 /bin/bash -l build.sh
