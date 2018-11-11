59fc522088f6a8652772353b9a422242cb670c82
Moritz Angermann <moritz.angermann@gmail.com>
2018-11-11 22:33:28 +0800

    Gah.. windows

--- .buildkite/windows.ps1
+++ .buildkite/windows.ps1
@@ -1,9 +1,9 @@
 Set-PSDebug -Trace 1
 
-$env:PATH="$env:PATH;D:\ghc\ghc-8.2.2\bin;D:\stack"
+$env:PATH="$env:PATH;D:\ghc\ghc-8.2.2\bin;D:\stack;C:\tools\msys64\usr\bin;\C:\tools\msys64\bin"
 
 choco install msys2 -y
 
 refreshenv
 
-C:\tools\msys64\usr\bin\mintty.exe --nodaemon - /bin/env MSYSTEM=MINGW64 /usr/bin/bash -l build.sh
+env.exe MSYSTEM=MINGW64 bash.exe -l build.sh
