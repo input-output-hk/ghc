75c8ad6557fe633f62e758f4a617cc7709d58233
Moritz Angermann <moritz.angermann@gmail.com>
2018-11-12 10:43:09 +0800

    Install prerequisites

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -1,4 +1,7 @@
 echo "here we go!"
+pacman -S --needed tar bsdtar binutils autoconf make xz \
+                   libtool automake python python2 p7zip patch
+
 ./boot
 ./configure
 make binary-dist
