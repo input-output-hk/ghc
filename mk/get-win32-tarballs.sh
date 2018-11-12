e96ba39c648247c49d785f73b9622240ce9ea5d0
Moritz Angermann <moritz.angermann@gmail.com>
2018-11-12 15:51:07 +0800

    dos2unix if needed.

--- mk/get-win32-tarballs.sh
+++ mk/get-win32-tarballs.sh
@@ -64,8 +64,7 @@ download_file() {
 
     if test "$verify" = "1"
     then
-	grep "${dest_file}$" mk/win32-tarballs.md5sum
-        grep "${dest_file}$" mk/win32-tarballs.md5sum | md5sum -c - ||
+        grep "${dest_file}$" mk/win32-tarballs.md5sum | dos2unix | md5sum -c - ||
             fail "ERROR: ${description} appears to be corrupted, please delete it and try again."
     fi
 }
