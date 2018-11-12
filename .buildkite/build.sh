338726234370d1e2c344ccb5b96ea2df80847c15
Moritz Angermann <moritz.angermann@gmail.com>
2018-11-12 13:46:55 +0800

    meh...

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -7,19 +7,19 @@ pacman -S --needed --noconfirm \
 arch=x86_64 # or i386
 bitness=64 # or 32
 
-export PATH=$PATH:/mingw${bitness}/bin:/usr/local/bin
+export PATH=$PATH:/d/ghc/ghc-8.4.3/bin:/d/usr/local/bin
 
 # install ghc if not present
-if [ ! -f /mingw${bitness}/bin/ghc ]; then
-    curl -L https://downloads.haskell.org/~ghc/8.4.3/ghc-8.4.3-${arch}-unknown-mingw32.tar.xz | tar -xJ -C /mingw${bitness} --strip-components=1
+if [ ! -f /d/ghc/ghc-8.4.3/bin/ghc ]; then
+    curl -L https://downloads.haskell.org/~ghc/8.4.3/ghc-8.4.3-${arch}-unknown-mingw32.tar.xz | tar -xJ -C /d/ghc
 fi
-mkdir -p /usr/local/bin
-if [ ! -f /usr/local/bin/cabal ]; then
-    curl -L https://www.haskell.org/cabal/release/cabal-install-2.2.0.0/cabal-install-2.2.0.0-${arch}-unknown-mingw32.zip | bsdtar -xzf- -C /usr/local/bin
+mkdir -p /d/usr/local/bin
+if [ ! -f /d/usr/local/bin/cabal ]; then
+    curl -L https://www.haskell.org/cabal/release/cabal-install-2.2.0.0/cabal-install-2.2.0.0-${arch}-unknown-mingw32.zip | bsdtar -xzf- -C /d/usr/local/bin
 fi
-if [ ! -f /usr/local/bin/alex ]; then
+if [ ! -f /d/usr/local/bin/alex ]; then
     cabal update
-    cabal install -j --prefix=/usr/local alex happy
+    cabal install -j --prefix=/d/usr/local alex happy
 fi
 
 # make sure we are in the buildkite build checkout path
