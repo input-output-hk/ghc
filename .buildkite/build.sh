d73edc5726c1d4f06cad2727cafd5aeaac409d0f
Moritz Angermann <moritz.angermann@gmail.com>
2018-11-12 13:18:40 +0800

    Build!!

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -1,7 +1,9 @@
 echo "here we go!"
 pacman -S --needed --noconfirm \
-       tar bsdtar binutils autoconf make xz \
-       libtool automake python python2 p7zip patch
+       curl git tar bsdtar binutils autoconf make xz \
+       libtool automake python python2 p7zip patch \
+       ca-certificates mingw-w64-$(uname -m)-gcc \
+       mingw-w64-$(uname -m)-tools-git
 
 # make sure we are in the buildkite build checkout path
 cd "${BUILDKITE_BUILD_CHECKOUT_PATH}"
