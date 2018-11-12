0fdc6c58a11db9b8f12009f45d0c0ea64da086b1
Moritz Angermann <moritz.angermann@gmail.com>
2018-11-12 11:19:16 +0800

    No Confirm

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -1,6 +1,7 @@
 echo "here we go!"
-pacman -S --needed tar bsdtar binutils autoconf make xz \
-                   libtool automake python python2 p7zip patch
+pacman -S --needed --noconfirm \
+       tar bsdtar binutils autoconf make xz \
+       libtool automake python python2 p7zip patch
 
 ./boot
 ./configure
--- .buildkite/windows.ps1
+++ .buildkite/windows.ps1
@@ -1,13 +1,7 @@
 Set-PSDebug -Trace 1
 
-(Get-Item -PATH ".\.buildkite\build.sh").FullName
-
 choco install msys2 -y
-
 refreshenv
-
-$env:Path.split(';')
-(Get-Item -Path ".\").FullName
 
 $env:PATH="D:\ghc\ghc-8.2.2\bin;C:\tools\msys64\usr\bin;\C:\tools\msys64\bin"
 
