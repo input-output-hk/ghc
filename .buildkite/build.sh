d8f9e0618ccbed7492d2c23d708900d8fe4f000d
Moritz Angermann <moritz.angermann@gmail.com>
2018-11-12 13:15:02 +0800

    Build!

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -3,10 +3,9 @@ pacman -S --needed --noconfirm \
        tar bsdtar binutils autoconf make xz \
        libtool automake python python2 p7zip patch
 
-env
-
-echo ${ENV}
-
+# make sure we are in the buildkite build checkout path
+cd "${BUILDKITE_BUILD_CHECKOUT_PATH}"
+# let's build!
 ./boot
 ./configure
-make binary-dist
+make binary-dist -j4
