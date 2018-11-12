fa8a5302cdf9c372d8e148cdada24d4e28913978
Moritz Angermann <moritz.angermann@gmail.com>
2018-11-12 13:08:35 +0800

    What's my env?

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -3,6 +3,10 @@ pacman -S --needed --noconfirm \
        tar bsdtar binutils autoconf make xz \
        libtool automake python python2 p7zip patch
 
+env
+
+echo ${ENV}
+
 ./boot
 ./configure
 make binary-dist
