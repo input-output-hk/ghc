cfee6bd4f630a63099aa9144e749acc61f963bec
Moritz Angermann <moritz.angermann@gmail.com>
2018-11-12 10:49:37 +0800

    No stack

--- .buildkite/windows.ps1
+++ .buildkite/windows.ps1
@@ -5,6 +5,6 @@ choco install msys2 -y
 
 refreshenv
 
-$env:PATH="D:\ghc\ghc-8.2.2\bin;D:\stack;C:\tools\msys64\usr\bin;\C:\tools\msys64\bin"
+$env:PATH="D:\ghc\ghc-8.2.2\bin;C:\tools\msys64\usr\bin;\C:\tools\msys64\bin"
 
 env.exe MSYSTEM=MINGW64 bash.exe -l .\.buildkite\build.sh
