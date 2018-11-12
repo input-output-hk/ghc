c8b0134f8b6f6f38e08b20cf8d602f3350f836e2
Moritz Angermann <moritz.angermann@gmail.com>
2018-11-12 10:45:56 +0800

    Install some GHC as well...

--- .buildkite/windows.ps1
+++ .buildkite/windows.ps1
@@ -3,6 +3,7 @@ Set-PSDebug -Trace 1
 $env:PATH="$env:PATH;D:\ghc\ghc-8.2.2\bin;D:\stack;C:\tools\msys64\usr\bin;\C:\tools\msys64\bin"
 
 choco install msys2 -y
+choco install ghc --version 8.4.4 -y
 
 refreshenv
 
